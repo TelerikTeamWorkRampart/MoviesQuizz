61d4224d1401f72747275b42f8b9fab929e97d7b
Nanich87 <flareshadow@gmail.com>
2015-08-26 10:40:43 +0300

    base routing

--- MoviesQuizzProposal/src/scripts/app.js
+++ MoviesQuizzProposal/src/scripts/app.js
@@ -11,16 +11,23 @@
         var app = Sammy('#main-content', function () {
             this.get('#/', function () {
                 // home
+                console.log("home");
             });
             this.get('#/login', function () {
                 // login
+                console.log("login");
             });
             this.get('#/quizz', function () {
                 // start new quizz
+                console.log("quizz");
             });
             this.get('#/logout', function () {
                 // logout
+                console.log("logout");
             });
+            this.notFound = function () {
+                console.log("not found");
+            };
         });
 
         app.run('#/');
