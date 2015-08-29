e6bebe7e6512d42e66d607cb4db046acac172c6a
Nanich87 <flareshadow@gmail.com>
2015-08-29 08:42:02 +0300

    added nav menu corresponding to wireframes

--- source/scripts/globalView.js
+++ source/scripts/globalView.js
@@ -1,11 +1,9 @@
 import 'jquery';
 
-
-//This is where the global navigation and overall appearence of the site is created
-//All the other views need to attach their output to this layout
-var globalView = (function(){
+// This is where the global navigation and overall appearence of the site is created
+// All the other views need to attach their output to this layout
+var globalView = (function () {
     var globalViewInternal = Object.create({});
-
 
     Object.defineProperties(globalViewInternal, {
         // init: {
@@ -14,39 +12,56 @@ var globalView = (function(){
         //     }
         // },
         draw: {
-            //Example use of
+            // Example use of
             value: function () {
                 var $container = $('.container');
-                var $menu = $('<nav >').addClass('navbar navbar-default');
-                var $ul = $('<ul />').addClass('nav navbar-nav');
+                var $menuContainer = $('<nav >').addClass('navbar navbar-default');
+                var $leftMenu = $('<ul />').addClass('nav navbar-nav pull-left');
+                var $rightMenu = $('<ul />').addClass('nav navbar-nav pull-right');
 
-                $ul.append($('<li />').addClass('active').append($('<a href="#" />').text('item 1')));
-                $ul.append($('<li />').append($('<a href="#" />').text('item 2')));
-                $ul.append($('<li />').append($('<a href="#" />').text('item 3')));
-                $ul.append($('<li />').append($('<a href="#" />').text('item 4')));
+                $leftMenu.append($('<li />').addClass('active').append($('<a data-id="1" href="#" />').text('Home')));
+                $leftMenu.append($('<li />').append($('<a data-id="2" href="#" />').text('New Game')));
+                $leftMenu.append($('<li />').append($('<a data-id="3" href="#" />').text('Instructions')));
+                $leftMenu.append($('<li />').append($('<a data-id="4" href="#" />').text('Score Board')));
+                $leftMenu.append($('<li />').append($('<a data-id="5" href="#" />').text('Credits')));
 
+                $rightMenu.append($('<li />').append($('<a data-id="6" href="#" />').text('Login / Register')));
 
                 $container.append($('<h1 />').addClass('jumbotron').text('Movies Quizz'));
-                $menu.append($ul);
-                $container.append($menu);
+                $menuContainer.append($leftMenu);
+                $menuContainer.append($rightMenu);
+                $container.append($menuContainer);
 
                 $container.append($('<div />').addClass('gameBoard'));
                 $container.append($('<div />').addClass('panel-footer').text('footer goes here'));
-
-
             }
         },
         registerClickCallback: {
             value: function (callback) {
-                document.addEventListener('mousedown', function(ev){
-                    if (ev.target.nodeName === 'A'){
-                        callback(ev.target.innerHTML);
+                document.addEventListener('mousedown', function (ev) {
+                    // ev.buttons === 1 detects left mouse button clicks only
+                    if (ev.buttons === 1 && ev.target.nodeName === 'A') {
+                        var link = $(ev.target);
+                        var pageIndex = $(link).attr('data-id');
+                        var activeItem = $(link).parent();
+                        
+                        clearActiveItem();
+                        setActiveItem(activeItem);
+
+                        callback(pageIndex);
                     }
                 }, false);
             }
-        },
-    })
+        }
+    });
 
+    function clearActiveItem() {
+        $('li.active').removeClass('active');
+    }
+
+    function setActiveItem(item) {
+        $(item).addClass('active');
+    }
 
     return globalViewInternal;
 })();
