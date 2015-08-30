0560b69f8e9c6ff60f405fdd27da4f3f03c7f14d
Nanich87 <flareshadow@gmail.com>
2015-08-30 08:20:39 +0300

    scoreboardView updated

--- source/scripts/controller.js
+++ source/scripts/controller.js
@@ -4,14 +4,11 @@ import {movie} from 'scripts/movie';
 import {globalView} from 'scripts/globalView';
 import {gameboardTimelineView} from 'scripts/gameboardTimelineView';
 import {loginView} from 'scripts/loginView';
+import {scoreboardView} from 'scripts/scoreboardView';
 import {dataBase} from 'scripts/dataBase';
 
 
-
-
-
-
-var game = gameTimelineModel.init(player('STOYAN', 23, 12, 2,  3, 4, 1));
+var game = gameTimelineModel.init(player('STOYAN', 23, 12, 2, 3, 4, 1));
 game.movies.push(movie('HotShots', 'Unknown', ['cast', 'who Cares'], 1991, 2.8, 'http://ia.media-imdb.com/images/M/MV5BMTQ4Mjg2NjY4NV5BMl5BanBnXkFtZTcwMjgwMDU1MQ@@._V1_SX300.jpg'));
 game.gameboardMovies.push(movie('Rocky', 'Unknown', ['cast', 'who Cares'], 1980, 8.8, 'http://ia.media-imdb.com/images/M/MV5BMTY5MDMzODUyOF5BMl5BanBnXkFtZTcwMTQ3NTMyNA@@._V1_SX300.jpg'));
 console.log(game);
@@ -19,9 +16,12 @@ console.log(game);
 
 var view = globalView;
 var gameView = gameboardTimelineView;
-var auth = loginView;
+var scoreView = scoreboardView;
+var authView = loginView;
 
 
+// for scoreboardView
+var players = [];
 
 view.draw();
 
@@ -29,30 +29,32 @@ view.draw();
 //gameView.draw(game.gameboardMovies, game.movies[0]); // should always go after view.draw. The gameboardTimelineView expects 2 parameters: 1 is array of movies, 2 is a single current movie
 
 
-
 //Database test
 dataBase.register('JarJar', 'asdf', {score: 12});
 
 
-
 //callbacks
-function alertMe(input){
+function alertMe(input) {
     alert(input);
 }
 
 // this is for testing purposes only
-function showView(pageIndex){
-   switch(pageIndex){
+function showView(pageIndex) {
+    switch (pageIndex) {
         case "2":
             gameView.draw(game.gameboardMovies, game.movies[0]);
             break;
+        case "4":
+            console.log('score board');
+            scoreView.draw(players);
+            break;
         case "6":
-            auth.showRegisterForm();
+            authView.showRegisterForm();
             break;
     }
 }
 
 view.registerClickCallback(showView);
-auth.registerClickCallback(alertMe);
+authView.registerClickCallback(alertMe);
 
 
