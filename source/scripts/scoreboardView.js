c87286afb1a1bd0d33d525ce4e30994a6e8d9b18
Nanich87 <flareshadow@gmail.com>
2015-08-30 08:50:25 +0300

    scoreboardView updated

--- source/scripts/scoreboardView.js
+++ source/scripts/scoreboardView.js
@@ -6,16 +6,41 @@ var scoreboardView = (function () {
 
     Object.defineProperties(scoreboardViewInternal, {
         draw: {
-            value: function (players) {
+            value: function (highScoreData) {
                 var $container = $('.container');
                 var $contentTitle = $('<h2 />').text('Score Board');
 
-                // TODO: create high score table
+                var $highScoreTable = $('<table />').addClass('table');
+                var $headerRow = $('<tr />')
+                    .append($('<th />').text('Player Name'))
+                    .append($('<th />').text('Player High Score'))
+                    .append($('<th />').text('Player Games'));
+
+                $highScoreTable.append($headerRow);
+
+                var rows = [];
+
+                for (var i = 0; i < highScoreData.length; i++) {
+                    var $tableRow = $('<tr />').attr('colspan', 3);
+                    var $playerName = $('<td />').text(highScoreData[i].playerName);
+                    var $playerHighScore = $('<td />').text(highScoreData[i].playerHighScore);
+                    var $playerGames = $('<td />').text(highScoreData[i].playerGames);
+
+                    $tableRow
+                        .append($playerName)
+                        .append($playerHighScore)
+                        .append($playerGames);
+
+                    rows.push($tableRow);
+                }
+
+                $highScoreTable.append(rows);
 
                 $container
                     .find('div.gameBoard')
                     .empty()
-                    .append($contentTitle);
+                    .append($contentTitle)
+                    .append($highScoreTable);
             }
         },
         registerClickCallback: {
