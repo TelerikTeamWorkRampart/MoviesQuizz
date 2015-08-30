1d993ef4402fd8528e0e4120971d14016710a758
Nanich87 <flareshadow@gmail.com>
2015-08-30 08:20:06 +0300

    scoreboardView updated

--- source/scripts/scoreboardView.js
+++ source/scripts/scoreboardView.js
@@ -1,29 +1,40 @@
 import 'jquery';
 
-
-//Scoreboard - current rankings
-var scoreboardView = (function(){
+// information about current player, score and game progress
+var scoreboardView = (function () {
     var scoreboardViewInternal = Object.create({});
-
 
     Object.defineProperties(scoreboardViewInternal, {
         draw: {
-            //Example use of
-            value: function () {
-                // TODO:
+            value: function (players) {
+                var $container = $('.container');
+                var $contentTitle = $('<h2 />').text('Score Board');
+
+                // TODO: create high score table
+
+                $container
+                    .find('div.gameBoard')
+                    .empty()
+                    .append($contentTitle);
             }
         },
         registerClickCallback: {
             value: function (callback) {
-                document.addEventListener('mousedown', function(ev){
-                    if (ev.target.nodeName === 'A'){
-                        callback(ev.target.innerHTML);
+                document.addEventListener('mousedown', function (ev) {
+                    if (ev.buttons === 1 && ev.target.nodeName === 'A') {
+                        var link = $(ev.target);
+                        var pageIndex = $(link).attr('data-id');
+                        var activeItem = $(link).parent();
+
+                        clearActiveItem();
+                        setActiveItem(activeItem);
+
+                        callback(pageIndex);
                     }
                 }, false);
             }
-        },
-    })
-
+        }
+    });
 
     return scoreboardViewInternal;
 })();
