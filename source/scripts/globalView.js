2a00dc1fbc458fc26fc608e1e347d003ffbfb25c
Nanich87 <flareshadow@gmail.com>
2015-09-03 21:00:49 +0300

    nav links now have icons

--- source/scripts/globalView.js
+++ source/scripts/globalView.js
@@ -20,9 +20,35 @@ var globalView = (function () {
                 var $leftMenu = $('<ul />').addClass('nav navbar-nav pull-left');
                 var $rightMenu = $('<ul />').addClass('nav navbar-nav pull-right rightMenu');
 
-                $leftMenu.append($('<li />').addClass('active').append($('<a data-id="1" href="#" />').text('Home')));
-                $leftMenu.append($('<li />').append($('<a data-id="2" href="#" />').text('New Game')));
-                $leftMenu.append($('<li />').append($('<a data-id="3" href="#" />').text('Instructions')));
+                var $homeIcon = $('<span />').addClass('glyphicon').addClass('glyphicon-home');
+                $leftMenu.append(
+                    $('<li />')
+                        .addClass('active')
+                        .append(
+                        $('<a data-id="1" href="#" />')
+                            .append($homeIcon)
+                            .append(' Home')
+                    )
+                );
+
+                var $newGameIcon = $('<span />').addClass('glyphicon').addClass('glyphicon-film');
+                $leftMenu.append(
+                    $('<li />').append(
+                        $('<a data-id="2" href="#" />')
+                            .append($newGameIcon)
+                            .append(' New Game')
+                    )
+                );
+
+                var $instructionsIcon = $('<span />').addClass('glyphicon').addClass('glyphicon-info-sign');
+                $leftMenu.append(
+                    $('<li />').append(
+                        $('<a data-id="3" href="#" />')
+                            .append($instructionsIcon)
+                            .append(' Instructions')
+                    )
+                );
+
                 $leftMenu.append($('<li />').append($('<a data-id="4" href="#" />').text('Score Board')));
                 $leftMenu.append($('<li />').append($('<a data-id="5" href="#" />').text('Credits')));
 
@@ -42,17 +68,28 @@ var globalView = (function () {
         },
         userUpdate: {
             value: function (player) {
-                var $rMenu = $('.rightMenu');
-                if(player === 'guest'){
+                var $rightMenu = $('.rightMenu');
+                if (player === 'guest') {
 
-                    $rMenu.empty();
-                    $rMenu.append($('<li />').append($('<a data-id="6" href="#" />').text('Login')));
-                    $rMenu.append($('<li />').append($('<a data-id="7" href="#" />').text('Register')));
+                    $rightMenu.empty();
+                    $rightMenu.append($('<li />').append($('<a data-id="6" href="#" />').text('Login')));
+                    $rightMenu.append($('<li />').append($('<a data-id="7" href="#" />').text('Register')));
                 } else {
                     console.log(player);
-                    $rMenu.empty();
-                    $rMenu.append($('<li />').append($('<a data-id="4" href="#" />').text(player)));
-                    $rMenu.append($('<li />').append($('<a data-id="8" href="#" />').text('Logout')));
+                    $rightMenu.empty();
+                    //$rightMenu.append($('<li />').append($('<a data-id="4" href="#" />').text(player)));
+                    var $userIcon = $('<span />')
+                        .addClass('glyphicon')
+                        .addClass('glyphicon-user');
+                    $rightMenu.append(
+                        $('<li />').append(
+                            $('<p />')
+                                .addClass('navbar-text')
+                                .append($userIcon)
+                                .append(' ' + player)
+                        )
+                    );
+                    $rightMenu.append($('<li />').append($('<a data-id="8" href="#" />').text('Logout')));
                 }
             }
         }
